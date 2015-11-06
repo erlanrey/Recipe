5991d0e59759d57227b84dad05c62cf031925cd5
erlanrey <erlanbiyok@gmail.com>
2015-11-06 00:37:09 +0000

    create chef resources and add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,14 @@ Rails.application.routes.draw do
     member do
       post 'like'
     end
-  end
+  end 
+  
+  resources :chefs, except: [:new] 
+  get '/register', to: 'chefs#new'
+  
+  get '/login', to: 'logins#new'
+  post '/login', to: 'logins#create'
+  get '/logout', to: 'logins#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
